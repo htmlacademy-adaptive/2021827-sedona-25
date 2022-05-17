e67936455319f9198ba0dd356cc5693bcb4a4c98
Vladimir Dudko <v.dudko126@gmail.com>
2022-05-17 23:54:02 +0300

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,14 @@ import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
 
 // Styles
 
-export const styles = () => {
+const styles = () => {
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
@@ -19,24 +23,69 @@ export const styles = () => {
       csso()
     ]))
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
 
 // HTML
-export const html = () => {
+ const html = () => {
   return gulp.src('source/*.html')
   .pipe(htmlmin({ collapseWhitespace: true }))
-   .pipe(gulp.dest('source'));
+   .pipe(gulp.dest('build'));
 }
 
+// Scripts
+ const script = () => {
+  return gulp.src('source/js/*.js')
+   .pipe(terser())
+   .pipe(gulp.dest('build/js'));
+ }
 
+// Images
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'))
+}
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(gulp.dest('build/img'))
+}
+
+// SVG
+const svg = () =>
+ gulp.src (['source/img/*svg', '!source/img/icons/*svg'])
+ .pipe(svgo())
+ .pipe(gulp.dest('build/img'));
+
+ const sprite = () => {
+ return gulp.src('source/img/icons/*svg')
+  .pipe(svgo())
+  .pipe(
+    svgstore({
+      inlineSvg: true
+ }))
+   .pipe(rename('sprite.svg'))
+   .pipe(gulp.dest('build/img'));
+ }
+
+// WebP
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(
+    squoosh({
+      webp: {},
+    })
+  )
+  .pipe(gulp.dest('build/img'));
+}
 
 // Server
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -54,5 +103,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  html, styles, server, watcher
 );
